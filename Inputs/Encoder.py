c42605ee0b3f12cbfd8b9bf53c3085d5989a9ca6
AlexBulavin <bulavin1970@icloud.com>
2022-01-13 08:48:21 +0300

    Normolize encoder values from -100:100 to 0-100 and tex positioning

--- Inputs/Encoder.py
+++ Inputs/Encoder.py
@@ -15,15 +15,20 @@ while True:
     potentiometer = cv2.imread("../Resources/Potentiometer.jpg")
     print(result[0], result[1])
     try:
-        val = str(int(result[1]) + 100)
+        val = str(int((int(result[1]) + 100)/2))
         valButton = result[0]
 
-        cv2.putText(potentiometer, val.zfill(3), (270, 280), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 3)
+        if int(val) < 10:
+            cv2.putText(potentiometer, val.zfill(1), (300, 280), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 3)
+        elif int(val) >= 10 & int(val) < 100:
+            cv2.putText(potentiometer, val.zfill(2), (283, 280), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 3)
+        elif int(val) >= 100:
+            cv2.putText(potentiometer, val.zfill(3), (260, 280), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 3)
 
         if val != '0':
             # -100 to +100
             # -90 to 270
-            val = np.interp(int(val), [0, 200], [-90, 270])
+            val = np.interp(int(val), [0, 100], [-90, 270])
             cv2.ellipse(potentiometer, (320, 265), (131, 131), 0, -90, val, (255, 180, 0), 27)
             #Вписать элипс (потому, что он позволяет строить арки) в имидж с центром 320х265, осями 131, 131, нулевой угол 0 сместить на -90 град (начать сверху)
             #преобразованными данными val, цветом (255, 180, 0), толщиной 27
